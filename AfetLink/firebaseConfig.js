b36904413e0452938292ff227977c282852f5320
orucfatih <orucfatih4545@gmail.com>
2025-04-08 23:30:55 +0300

    Firebase config ve .env dosyası

---
+++ AfetLink/firebaseConfig.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID
+} from '@env';
+
+//authentication çalışıyor ama firestore database çalışmıyor.
+//firestore database kuralları değişmeli.
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
+  };
+  
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const auth = initializeAuth(app, {
+persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export const db = getFirestore(app);
+
+export default app;
+
